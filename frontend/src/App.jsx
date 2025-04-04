6f5d183aa614c202dd61d590f3a1ffa2f617eab7
pipil <jmh4011@gmail.com>
2025-04-05 04:09:22 +0900

    react-vite 설치 후 구동

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white textblack">
+      <h1 className="text-2xl font-bold">hello, Tailwind!</h1>
+    </div>
+  )
+}
+
+export default App
